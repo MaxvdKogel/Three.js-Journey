941f865267ea332b423b9c41889d34e61e019b4a
MaxvdKogel <maxvdkogel123@gmail.com>
2021-11-22 12:30:58 +0100

    Animations 06

--- src/script.js
+++ src/script.js
@@ -1,14 +1,32 @@
 import './style.css'
 import * as THREE from 'three'
+import gsap from 'gsap'
 
 //scene
 const scene = new THREE.Scene()
 
-//red cube
-const geometry = new THREE.BoxGeometry(1,1,1)
-const material = new THREE.MeshBasicMaterial({color: 0xff0000})
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+//objects
+const group = new THREE.Group()
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+cube2.position.x = -2
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+cube3.position.x = 2
+
+group.add(cube1, cube2, cube3)
 
 //sizes
 const sizes = {
@@ -27,4 +45,41 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+//clock
+const clock = new THREE.Clock()
+
+//animations
+const tl = gsap.timeline({
+    repeat: -1,
+    defaults: {
+        duration: 1.5
+    }
+})
+
+tl.to(cube2.position, {
+    x: 2
+}).to(cube3.position, {
+    x: -2
+}, "-=1.5").to(cube2.position, {
+    x: -2
+}).to(cube3.position, {
+    x: 2
+}, "-=1.5")
+
+
+const tick = () => {
+    //time
+    const elapsedTime = clock.getElapsedTime()
+
+    //update objects
+    cube1.position.y = Math.sin(elapsedTime * 2) * 1.5
+    cube1.position.x = Math.cos(elapsedTime * 2) * 1.5
+
+    //render
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+
+tick()
